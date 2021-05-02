f72462d233b294079b93dbd3f95bc0abfbfbb841
Vmalgotra <32836184+Vmalgotra@users.noreply.github.com>
2021-05-02 12:45:37 +0530

    Update BehaviourParameterization.java

--- BehaviourParameterization.java
+++ BehaviourParameterization.java
@@ -6,23 +6,56 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 public class FP04Behavioural {
+	
+	
 	public static void main(String[] args) {
-     List <Integer> number = List.of(1,21,3,22,12,2,3,4,5,5);
-     number.stream()
-     .filter( x -> x%2==0)
-     .forEach(System.out::println);
-     number.stream()
-     .filter( x -> x%2!=0)
-     .forEach(System.out::println);
-     // So apart from behaviour i.e. the logic, everything is duplicate. So we can use behaviour parameterization to avoid
-     // duplicate code. here the logic/behaviour of the method as an arguement to the method
-     filterAndPrint(number, x -> x%2==0);
-     filterAndPrint(number, x -> x%2!=0);
+		
+		
+    	 List <Integer> number = List.of(1,21,3,22,12,2,3,4,5,5);
+		
+   	 number.stream().filter( x -> x%2==0).forEach(System.out::println);
+    
+	 number.stream().filter( x -> x%2!=0).forEach(System.out::println);
+		
+	//Steps to use behaviour paramterization in eclipse
+		
+	//select the lambda function and click refactor --> extract variable
+        //then select the complete line of code 'number.strema().....foreach() and then right click extract method.
+        // now select the functional interface name and refactor --> inline
+        //through behaviour parameterization	
+		
+        // So apart from behaviour i.e. the logic, everything is duplicate. So we can use behaviour parameterization to avoid
+        // duplicate code. here the logic/behaviour of the method as an arguement to the method
+   
+	filterAndPrint(number, x -> x%2==0);
+     	filterAndPrint(number, x -> x%2!=0);
+		
+		
+	// without behaviour parameterization
+        number.stream().map( x -> x*x).forEach(System.out::println);
+        number.stream().map( x -> x*x*x).forEach(System.out::println);
+
+        multiandprint(number, x -> x*x*x);
+        multiandprint(number, x -> x*x);	
+		
 	}
+	
 	private static void filterAndPrint(List<Integer> number, Predicate<Integer> evenpredicate) {
 		number.stream()
 		 .filter(evenpredicate)
 		 .forEach(System.out::println);
 	}
+	
+
+	}
+	private static void multiandprint(List<Integer> number, Function<Integer, Integer> mapper) {
+		number.stream()
+		 .map( mapper)
+		 .forEach(System.out::println);
+	}
+	
+	
+	
+	
 }
 ///behaviur parameterization -- passing behaviour as a arguement of function
